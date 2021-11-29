f71c6a149657affb78b2583f9b26a463555ceecf
unknown <ksng@kth.se>
2021-11-29 21:31:21 +0100

    rename

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 //Loads modules (https://stackoverflow.com/questions/9901082/what-is-this-javascript-require)
 const Discord = require('discord.js');
 
-let data = require('./env.json');
+let data = require('./config.json');
 var token = data.data.token;
-
-//Token stored somewhere else, for safety.
-/*token = fetch('./env.json')
-    .then(response => response.json())
-    .then(data => data.token)
-    .catch(error => console.log("Can't find token"));
-*/
 
 const client = new Discord.Client({
     allowedMentions:{
@@ -30,6 +23,7 @@ client.on("ready", () =>{
     console.log("Bot deployed");
 })
 
+//Bot actions
 client.on("messageCreate", async message => {
     if(message.content === "!ping"){
         message.channel.send("Hi!");
