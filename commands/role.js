afac88711e579e58e2ccb6e2440e8de9f2605557
unknown <ksng@kth.se>
2022-01-17 11:34:08 +0100

    added messageReactionAdd and messageReactionRemove for role selection

--- commands/role.js
+++ commands/role.js
@@ -2,54 +2,65 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
-//Role reaction code.
-active_reactions = ['💚'];
+//Role reaction.
+//In order: Marine, VG, MTG, F&B, Programming, M&T, PR, TT.
+//var reactions = ['💚','🎮','🎴','🥧','💻','🎥','📯','🎲'];
+
+//To add role:
+//Edit/Add role to config.json, "openRoles"
+//Add an emoji in "reactions"
+//Go to messageReactionAdd and add a new case.
+//Go to messageReactionRemove and add a new case.
+//The roles have to be ordered in config.json
+//e.g. if the order is "A, C, B" in config.json,
+//The reaction order will be
+// 1. gives role 'A'
+// 2. gives role 'C'
+// 3. gives role 'B'
+
+var test = ['💚','🎮','🎴','🥧'];
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('role')
 		.setDescription('Creates option to choose role with emojis!'),
+    reactions: ['💚','🎮','🎴','🥧','💻','🎥','📯','🎲'],
 	async execute(interaction) {
+        // Gets all roles
+        //let rolemap = await interaction.guild.roles.cache.sort((a, b) => b.position - a.position).map(r => r.name).join(",");
+
         const embed = new MessageEmbed()
         .setColor('#ffffff')
-        .setTitle('Roles')
-        .setDescription('Test: \n\n'
-            + '1\n'
-            + '2');
+        .setTitle('Select Roles')
+        .setDescription(`Reacting to the following roles will give you the desired role.\n
+            You will receive information and news depending on the role you are assigned to: \n\n
+            
+            💚: 'Marine'! Everything and anything related to ITK will reach you!\n
+            🎮: 'Gamer-Guru'! You like video games? Want to play with others? Then this one is for you!\n
+            🎴: 'Court Wizard'! Want to discuss which the coolest decks in MTG are? Cast Lightning bolts on people's face? Then react to this one!\n
+            🥧: 'Chef'! What's the most exquisite cuisine! React this one to discuss with others!\n
+            💻: 'Programmer' Is sleep-sort the best sort? Want to discuss the time-complexity of it? Then this one is for you!\n
+            🎥: 'Movie Master' Is sand the best spice? Or maybe young Skywalker was right... React here to discuss!\n
+            📯: 'PR' Want to influence people of ITK? Well... reacting here gives you the power to do it!\n
+            🎲: 'Tabletopper' Like to discuss opening moves in chess? Perhaps build a rail-empire in Ticket for Ride? React here!\n
+        `);
         
+
         //deferReply and deleteReply is there to make it so bot replies with showing the command.
         //https://www.reddit.com/r/Discordjs/comments/r1kwg4/reply_without_replying/
         await interaction.deferReply();
         await interaction.deleteReply();
 
         //https://stackoverflow.com/questions/52457004/empty-message-problem-with-discord-js-embeds/52461599
+        //React with all reactions representing the ITK groups.
         const msg = await interaction.channel.send({embeds: [embed]});
-        msg.react('💚');
+        //msg.react('💚');
+        
+        for(let i = 0; i < test.length; i++){
+        //for(let i = 0; i < reactions.length; i++){
+            //msg.react(reactions[i]);
+            msg.react(test[i]);
+        }
+        
 
-        /*
-        //includecheck: checks if relevant emoji has been reacted with
-        //idcheck: checks that it's not the author (Nat)
-        const filter = (reaction, user) => {
-            const includecheck = active_reactions.includes(reaction.emoji.name);
-            const idcheck = user.id !== msg.author.id;
-            return includecheck && idcheck;
-        };
-
-        const collector = msg.createReactionCollector({ filter });
-
-        collector.on('collect', async (reaction, user) => {
-            //const role = await msg.guild.roles.fetch(931929374188523580);
-            //const role = interaction.options.getRole("Marine");
-            //const role = await msg.guild.roles;
-            
-            //role.array.forEach(role => { console.log(role.name, role.id)          
-            //});
-            //console.log(role);
-            //msg.guild.members.fetch(user.id).then(member => {
-            //    member.roles.add(role);
-            //})
-            //reaction.message.guild.members.cache.get(user.id).roles.add(role);
-            reaction.message.guild.members.cache.get(user.id).roles.add("931929374188523580");
-        })
-        */
 	},
 };
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,36 +1,36 @@
 //https://discordjs.guide/creating-your-bot/event-handling.html#individual-event-files
 
 const roleCommand = require(`../commands/role.js`)
+const messageReactionAdd = require(`./messageReactionAdd.js`)
 const config = require(`../config.json`)
 
-//Reaction add
+//Reaction remove
 //In order: Marine, VG, MTG, F&B, Programming, M&T, PR, TT.
 //reactions = ['💚','🎮','🎴','🥧','💻','🎥','📯','🎲'];
-
 module.exports = {
-	name: 'messageReactionAdd',
+	name: 'messageReactionRemove',
 	async execute(reaction, user) {
 		//If the reactor is the bot - return
 		let clientId = config.clientId;
 		if(user.id === clientId) return;
-		//Get the reactions and channelId and available roles.
-		let reactions = roleCommand.reactions;
+		//Get role-reactions and channelId
 		let channelId = config.roleChannelId;
-		let openRoles = config.openRoles;
+		let reactions = roleCommand.reactions;
 		//If the reaction not part of the role-reactions, return.
 		if(!reactions.includes(reaction.emoji.name)) return;
 		
-        // Gets all roles
-        let rolemapName = await reaction.message.guild.roles.cache.sort((a, b) => b.position - a.position).map(r => r.name).join(",");
+		// Gets all roles
+		let rolemapName = await reaction.message.guild.roles.cache.sort((a, b) => b.position - a.position).map(r => r.name).join(",");
 		let rolemapNamelist;
 
 		if (!rolemapName) console.log("No roles");
 		else{
 			rolemapNamelist = rolemapName.split(",");
 		}
-
+		
 		//Filter out relevant roles from all roles.
-		var relevant_roles_name = [];
+		let relevant_roles_name = [];
+		let openRoles = config.openRoles;
 		for(let i = 0; i < openRoles.length; i++){
 			if (rolemapNamelist.includes(openRoles[i])){
 				relevant_roles_name.push(openRoles[i]);
@@ -40,23 +40,23 @@ module.exports = {
 		//Find the member of reacted to the message.
 		const m = reaction.message.guild.members.cache.find(member => member.id === user.id);
 
-		//If the reaction was in the correct channel..
-		//If there is a new role, add new case for it.
+		//If the removed reaction was in the correct channel..
+		//If there is a new role, add new case for it (removed).
 		if(channelId === reaction.message.channelId){
-			let givenrole;
+			let removedrole;
 			let cache = reaction.message.guild.roles.cache;
 			//Depending on the reaction, set the role. The order of the reactions can be seen in 'role.js'
 
 			switch(reaction.emoji.name){
-				//ADD NEW CASE BELOW
-				case reactions[0]: givenrole = cache.find(role => role.name === relevant_roles_name[0]); break;
-				case reactions[1]: givenrole = cache.find(role => role.name === relevant_roles_name[1]); break;
-				case reactions[2]: givenrole = cache.find(role => role.name === relevant_roles_name[2]); break;
-				case reactions[3]: givenrole = cache.find(role => role.name === relevant_roles_name[3]); break;
+				case reactions[0]: removedrole = cache.find(role => role.name === relevant_roles_name[0]); break;
+				case reactions[1]: removedrole = cache.find(role => role.name === relevant_roles_name[1]); break;
+				case reactions[2]: removedrole = cache.find(role => role.name === relevant_roles_name[2]); break;
+				case reactions[3]: removedrole = cache.find(role => role.name === relevant_roles_name[3]); break;
 			}
 
-			m.roles.add(givenrole);
-			m.send(`You have now the role of "${givenrole.name}."`);
+			m.roles.remove(removedrole);
+			m.send(`You no longer have the role of "${removedrole.name}."`);
 		}
+		
 	},
 };
