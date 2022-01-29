1b9ef035c219229d68ecf7c8644e85f8f1204f42
unknown <ksng@kth.se>
2022-01-29 22:56:28 +0100

    Rewrite role. Added new !addrole which adds role. Needs <emoji>, <messageid> and <role>. Needs removerole and change to messageReactionAdd and messageReactionRemove

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,12 +1,26 @@
 //https://discordjs.guide/creating-your-bot/event-handling.html#individual-event-files
 
+const { MessageEmbed } = require('discord.js');
+const config = require('../config.json');
+
 //On message create
 //role message id 936989066115829822
 //message id length: 18
 //Point is to have <emoji>: <role> | (description structure?)
+
+//https://discordjs.guide/popular-topics/embeds.html#resending-and-editing
+//Editing seems to not update the cache, which we will possibly need to remember who reacted to who on add role.
+//So editing the embed might require a whole new embed, if we use embed.
+
+//https://github.com/AnIdiotsGuide/discordjs-bot-guide/blob/master/coding-guides/using-emojis.md
+//For how emoji works.
+
+//names need to be all lower case - Discord API
+//role message id 936989066115829822
+
 module.exports = {
 	name: 'messageCreate',
-	execute(message) {
+	async execute(message) {
 		if(message.content.startsWith("!addrole")){
 
 			//Extracts the messageId in the message content.
@@ -24,22 +38,41 @@ module.exports = {
 			let regexRoleMention = new RegExp('<@!*&*[0-9]+>', 'g');
 			let roleMention = message.content.match(regexRoleMention);
 
+			//Get the role message
+			const msg = await message.channel.messages.fetch(messageId[0]);
 
+			//Get the title and the description of the embed
+			const oldTitle = msg.embeds[0].title;
+			var oldDescription = msg.embeds[0].description;
+
+			//Concatenating new emoji and new role
+			oldDescription = oldDescription.concat("\n", emojiReaction[0], ":", roleMention[0], "\n");
+
+			//Creating a new embed
+			const newEmbed = new MessageEmbed()
+			.setColor(config.NATgreen)
+			.setTitle(oldTitle)
+			.setDescription(oldDescription)
+			;
 			
-			//message.channel.send(emojiReaction[0]);
-			message.channel.send(roleMention[0]);
-			const msg = message.channel.messages.fetch(messageId[0]);
-			console.log(msg);
-			console.log("msg.content");
-			console.log(msg.content);
-	
-			//await interaction.reply(t);
-            //message.channel.send("pong!");
+			//Editing the embed
+			const m = await msg.edit({embeds:[newEmbed]})
+
+			message.channel.send("Adding emoji: " + emojiReaction[0]);
+			message.channel.send("Adding role: " + roleMention[0]);
+			message.channel.send("Reacting with the emoji: " + emojiReaction[0]);
+
+			//React with the added emoji
+			m.react(emojiReaction[0]);
+        }
+
+		else{
+			//message.channel.send("pong!");
 			/*
 		    message.reply({
 			    content: 'pong!'
 		    })
 			*/
-        }
+		}
 	},
 };
